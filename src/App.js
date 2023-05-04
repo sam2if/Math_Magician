ca4caaaf28c67843b47a3fc2c6e7e166b1ffcf6d
sami <samif3443@gmail.com>
2023-05-04 13:32:24 -0700

    Fixed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Affiche from './components/ZoumCalculator';
+import './components/Calculator.css';
+
+function App() {
+  return (
+    <div className="main">
+      <Affiche />
+    </div>
+
+  );
+}
+export default App;
