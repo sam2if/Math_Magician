b35ba7c48c8505ffafd7b0ac0bd107e8aba52688
sami <samif3443@gmail.com>
2023-05-04 12:55:43 -0700

    Fix linter errors

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calculator from './Components/calculator';
+
+function App() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+export default App;
