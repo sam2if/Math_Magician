9a5d84b7ba77be0a8afdb1d81cca10192a601c5c
sami <samif3443@gmail.com>
2023-05-06 00:35:19 -0700

    Finish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Calculator from './components/MyCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
